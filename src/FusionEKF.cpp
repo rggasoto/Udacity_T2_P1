7195d5673742cdf518f92f5fa0f6743ad2859583
Renato Girardi Gasoto <re.gasoto@gmail.com>
2017-04-17 12:38:35 -0400

    Final project

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -125,18 +125,11 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 	 *  Prediction
 	 ****************************************************************************/
 
-	 /**
-	  TODO:
-		* Update the state transition matrix F according to the new elapsed time.
-		 - Time is measured in seconds.
-		* Update the process noise covariance matrix.
-		* Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
-	  */
+	
 	double dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
 	
 	
-	//cout << "dt = " << measurement_pack.timestamp_ - previous_timestamp_ << " " << dt << endl;
-	//cin >> dt;
+	
 	previous_timestamp_ = measurement_pack.timestamp_;
 	VectorXd z;
 
@@ -161,11 +154,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 	 *  Update
 	 ****************************************************************************/
 
-	 /**
-	  TODO:
-		* Use the sensor type to perform the update step.
-		* Update the state and covariance matrices.
-	  */
+	
 	switch (measurement_pack.sensor_type_) {
 	case MeasurementPackage::RADAR:
 		z = VectorXd(3);
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -20,21 +20,13 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
-	x_ = F_ * x_;
+  	x_ = F_ * x_;
 	MatrixXd Ft = F_.transpose();
 	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
-
+ 
 	VectorXd z_pred = H_ * x_;
 	VectorXd y = z - z_pred;
 	MatrixXd Ht = H_.transpose();
@@ -51,30 +43,21 @@ void KalmanFilter::Update(const VectorXd &z) {
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
-	//std::cout << "update y" << std::endl;
+ 
 	VectorXd y = z - Tools::h(x_);
-	//std::cout << "Ht" << std::endl;
 	MatrixXd Ht = Hj_.transpose();
-	//std::cout << "S" << std::endl;
 	MatrixXd S = Hj_ * P_ * Ht + R_;
-	//std::cout << "Si" << std::endl;
 	MatrixXd Si = S.inverse();
-	//std::cout << "PHt" << std::endl;
 	MatrixXd PHt = P_ * Ht;
-	//std::cout << "K" << std::endl;
 	MatrixXd K = PHt * Si;
 
 	//new estimate
-	//std::cout << "New Estimate" << std::endl;
+	
 	x_ = x_ + (K * y);
 	long x_size = x_.size();
 	MatrixXd I = MatrixXd::Identity(x_size, x_size);
-	//std::cout << "P" << x_size<<std::endl;
+	
 	P_ = (I - K * Hj_) * P_;
-	//std::cout << "Done" << std::endl;
+	
 
 }
